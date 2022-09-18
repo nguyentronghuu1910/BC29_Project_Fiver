3283f5b69cf93c620898dfe2a039ec5a498b3018
hongthanh711 <phthanh1999vvk@gmail.com>
2022-09-18 15:20:34 +0700

    config

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,5 +1,27 @@
 import React from 'react'
+import { Input } from 'antd'
+import './index.scss'
+import { useAsync } from '../../hooks/useAsync'
+import { fetchJobListApi } from '../../services/job'
+import { useNavigate } from 'react-router-dom'
 
-export default function Search() {
-    return <div>search</div>
+export default function Search(): JSX.Element {
+    const navigate = useNavigate()
+
+    const { Search: SearchAntd } = Input
+
+    const onSearch = (value: string) => {
+        navigate(`/search/${value}`)
+    }
+
+    return (
+        <div className="w-25">
+            <SearchAntd
+                className="search"
+                placeholder="What service are you looking for"
+                onSearch={onSearch}
+                enterButton
+            />
+        </div>
+    )
 }
